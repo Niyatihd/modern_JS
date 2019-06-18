95f95e60dad152ec0a47ad6dcb6466572f5d9484
Niyati Desai <niyatihd@gmail.com>
2019-06-18 11:23:31 -0700

    console log

---
+++ js_sandbox/app.js
@@ -0,0 +1,22 @@
+// alert("Hello!!");
+
+//Note:
+// Single line comment
+/*
+Multi line comment
+*/
+console.log("abc");
+console.log(123);
+console.log(true);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error("this is an error");
+console.clear();
+console.warn("this is a warning");
+//The piece of code below gives us the time it takes to log all "hello world"s
+console.time("hello");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.timeEnd("hello");
